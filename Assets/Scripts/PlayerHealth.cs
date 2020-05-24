9ee27292f4d4b1fae784e1030bc69316aa1082bd
RaulM99 <RaulM99@users.noreply.github.com>
2020-05-24 17:25:07 -0500

    No he terminado los scripts
    No presten mucha atencion a esos scripts, pero los voy a poner porque como que me toca

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth = 0;
+    public int maxHealth = 100;
+    public float hbLength = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        hbLength = Screen.with / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeHealth(0);
+    }
+
+    void GUI()
+    {
+        GUI.Box(new Rect(10, 10, hbLength, 30), currentHealth + " / " + maxHealth);
+    }
+
+    public void ChangeHealth (int heealth)
+    {
+        hbLength = (Screen.width / 2) * (currentHealth / (float)maxHealth);
+    }
+}
---
+++ Assets/Scripts/ZombieAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAttack : MonoBehaviour
+{
+    public float maxDistance;
+    public float coolDownTimer;
+    public PlayerHealth ph;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    
+}
